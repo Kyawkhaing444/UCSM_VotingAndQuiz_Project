033016b00fa1f6e54b519ed8e14f77a281b28c2a
Kyaw Kyaw Khaing <kyaw@kyaw-linux>
2019-01-22 06:27:14 +0630

    Version_1,2

---
+++ UCSM_Voting/resources/views/Admin/dashboard/index.blade.php
@@ -0,0 +1,66 @@
+@extends('layouts.Admin.dashboard')
+
+@section('content')
+<!--Main layout-->
+<main class="pt-5 mx-lg-5">
+  <div class="container-fluid mt-5">
+
+    <!-- Heading -->
+    <div class="card mb-4 wow fadeIn">
+
+      <!--Card content-->
+      <div class="card-body d-sm-flex justify-content-between">
+        <h4 class="mb-2 mb-sm-0 pt-1">
+          <span style="font-weight: bolder;">Dashboard</span>
+        </h4>
+
+        <form class="d-flex justify-content-center">
+          <!-- Default input -->
+          <input type="search" placeholder="Search" aria-label="Search" class="form-control">
+          <button class="btn btn-primary btn-sm my-0 p" type="submit">
+            <i class="fas fa-search"></i>
+          </button>
+
+        </form>
+
+      </div>
+
+    </div>
+    <!-- Heading -->
+
+    <!--Grid row-->
+    <div class="row wow fadeIn">
+
+      <!--Grid column-->
+      <div class="col-md-12 mb-4">
+
+        <!--Card-->
+        <div class="card">
+
+          <!--Card content-->
+          <div class="card-body">
+
+            <canvas id="myChart"></canvas>
+
+          </div>
+
+        </div>
+        <!--/.Card-->
+
+      </div>
+      <!--Grid column-->
+    </div>
+  </div>
+        <!--/.Card-->
+
+      </div>
+      <!--Grid column-->
+
+    </div>
+    <!--Grid row-->
+
+  </div>
+</main>
+<!--Main layout-->
+
+@endsection
--- UCSM_Voting/routes/web.php
+++ UCSM_Voting/routes/web.php
@@ -14,3 +14,7 @@
 Route::get('/', function () {
     return view('welcome');
 });
+
+Route::get('AdminDashbard', function(){
+   return view('Admin.dashboard.index');
+});
