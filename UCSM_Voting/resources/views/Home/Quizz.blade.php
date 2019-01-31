19ee1124c538098c9543be984ff28c1c713b3ab8
Kyaw Kyaw Khaing <kyaw@kyaw-linux>
2019-02-01 05:18:41 +0630

    Version1_4_K

--- UCSM_Voting/resources/views/Home/Quizz.blade.php
+++ UCSM_Voting/resources/views/Home/Quizz.blade.php
@@ -100,24 +100,24 @@
                                     <!-- Default unchecked -->
                                         <!-- Group of default radios - option 1 -->
                                         <div class="custom-control custom-radio">
-                                                <input type="radio" class="custom-control-input" id="defaultGroupExample1" name="{{ $q->item1 }}">
+                                                <input type="radio" class="custom-control-input" id="{{str_replace(' ', '', $q->item1)}}" name="{{ $q->item1 }}">
                                                 <label class="custom-control-label" for="defaultGroupExample1">{{ $q->item1 }}</label>
                                             </div>
 
                                             <!-- Group of default radios - option 2 -->
                                             <div class="custom-control custom-radio">
-                                                <input type="radio" class="custom-control-input" id="defaultGroupExample2" name="{{ $q->item1 }}">
+                                                <input type="radio" class="custom-control-input" id="{{str_replace(' ', '', $q->item2)}}" name="{{ $q->item2 }}">
                                                 <label class="custom-control-label" for="defaultGroupExample2">{{ $q->item2 }}</label>
                                             </div>
 
                                             <!-- Group of default radios - option 3 -->
                                             <div class="custom-control custom-radio">
-                                                <input type="radio" class="custom-control-input" id="defaultGroupExample3" name="{{ $q->item1 }}">
+                                                <input type="radio" class="custom-control-input" id="{{str_replace(' ', '', $q->item3)}}" name="{{ $q->item3 }}">
                                                 <label class="custom-control-label" for="defaultGroupExample3">{{ $q->item3 }}</label>
                                             </div>
                                              <!-- Group of default radios - option 3 -->
                                             <div class="custom-control custom-radio">
-                                                    <input type="radio" class="custom-control-input" id="defaultGroupExample4" name="{{ $q->item1 }}">
+                                                    <input type="radio" class="custom-control-input" id="{{str_replace(' ', '', $q->item4)}}" name="{{ $q->item4 }}">
                                                     <label class="custom-control-label" for="defaultGroupExample4">{{ $q->item4 }}</label>
                                                 </div>
                                 </div>
